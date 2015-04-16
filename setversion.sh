512c1fca764c53494635c730d9ca8f7f91fc75e9
Henrik Huttunen <henrik.huttunen@gmail.com>
2015-04-16 17:36:26 +0300

    version based on git hash

--- setversion.sh
+++ setversion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export version=`hg id -n`
-echo "Replace version number in $1 with hg id -n " $version
+export version=`git rev-parse HEAD`
+echo "Replace version number in $1 with git rev-parse HEAD " $version
 perl -pi -e 's/{VERSION}/'$version'/g' production/*.html
